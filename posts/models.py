8dfeeea94d99f74a998a3dc67736a3fc71c60144
Paul Onteri <paul@inuua.net>
2021-03-24 00:35:36 +0300

    tag model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    count = models.IntegerField(null=True, blank=True)
+    taxonomy = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.slug}"
